06de72501027113f885d540bd4ac40a503ba3809
AzrHaru <brnmondia@gmail.com>
2024-03-18 16:23:39 +0800

    third

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,99 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width: 1440, height: 3941, position: 'relative', background: '#333333'}}>
+  <img style={{width: 1440, height: 608.45, left: 0, top: 70, position: 'absolute', borderTopLeftRadius: 20, borderTopRightRadius: 20}} src="../img/20240305_134357_0000 1.png" />
+  <div style={{width: 287, height: 64, left: 576, top: 788, position: 'absolute'}}>
+    <div style={{width: 257, height: 10, left: 15, top: 0, position: 'absolute', background: '#007FFF', borderRadius: 20}} />
+    <div style={{width: 200, height: 5, left: 44, top: 59, position: 'absolute', background: '#007FFF', borderRadius: 20}} />
+    <div style={{width: 287, height: 19, left: 0, top: 25, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 32, fontFamily: 'Exo', fontWeight: '600', wordWrap: 'break-word'}}>Featured Products</div>
+  </div>
+  <div style={{width: 400, height: 1000, left: 84, top: 920, position: 'absolute', background: '#005DB3', borderRadius: 20}} />
+  <div style={{width: 400, height: 1000, left: 956, top: 920, position: 'absolute', background: '#005DB3', borderRadius: 20}} />
+  <div style={{width: 400, height: 1000, left: 520, top: 920, position: 'absolute', background: '#005DB3', borderRadius: 20}} />
+  <img style={{width: 400, height: 293.98, left: 520, top: 956, position: 'absolute'}} src="https://via.placeholder.com/400x294" />
+  <img style={{width: 400, height: 293.98, left: 84, top: 956, position: 'absolute'}} src="https://via.placeholder.com/400x294" />
+  <img style={{width: 400, height: 332.83, left: 956, top: 935, position: 'absolute'}} src="https://via.placeholder.com/400x333" />
+  <div style={{width: 303, height: 585, left: 568, top: 1265, position: 'absolute'}}>
+    <div style={{width: 303, left: 0, top: 0, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 36, fontFamily: 'Exo', fontWeight: '600', wordWrap: 'break-word'}}>ROG Strix SCAR 18 (2024)</div>
+    <div style={{width: 269, left: 17, top: 135, position: 'absolute', textAlign: 'center', color: '#F5F5F5', fontSize: 20, fontFamily: 'Exo', fontWeight: '600', wordWrap: 'break-word'}}>Up to Intel® Core™ i9-14900HX<br/>Up to GeForce® RTX™ 4090 GPU<br/>Up to Windows 11 Pro<br/>Up to 18-inch, 2.5k Mini-LED, 16:10 (2560 x 1600, WQXGA), Refresh Rate: 250Hz/3ms<br/>ROG Nebula HDR<br/>Up to 2TB PCIe® 4.0 NVMe™ M.2 SSD</div>
+    <div style={{left: 58, top: 452, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 40, fontFamily: 'Exo', fontWeight: '700', wordWrap: 'break-word'}}>$2,999.99</div>
+    <div style={{width: 200, height: 60, left: 52, top: 525, position: 'absolute', background: '#007FFF', borderRadius: 16}}></div>
+    <div style={{left: 106, top: 533, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 32, fontFamily: 'Exo', fontWeight: '500', wordWrap: 'break-word'}}>Select</div>
+  </div>
+  <div style={{width: 303, height: 585, left: 133, top: 1265, position: 'absolute'}}>
+    <div style={{width: 303, left: 0, top: 0, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 36, fontFamily: 'Exo', fontWeight: '600', wordWrap: 'break-word'}}>2021 ROG Zephyrus S17</div>
+    <div style={{width: 269, left: 17, top: 135, position: 'absolute', textAlign: 'center', color: '#F5F5F5', fontSize: 20, fontFamily: 'Exo', fontWeight: '600', wordWrap: 'break-word'}}>Windows 10 Pro<br/>NVIDIA® GeForce RTX™ 3080 Laptop GPU<br/>11th Gen Intel® Core™ i9-11900H Processor<br/>17.3" WQHD (2560 x 1440) 16:9 165Hz<br/>2TB M.2 NVMe™ PCIe® 4.0 Performance SSD storage</div>
+    <div style={{left: 59, top: 452, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 40, fontFamily: 'Exo', fontWeight: '700', wordWrap: 'break-word'}}>$3,299.99</div>
+    <div style={{width: 200, height: 60, left: 51, top: 525, position: 'absolute', background: '#007FFF', borderRadius: 16}} />
+    <div style={{left: 106, top: 533, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 32, fontFamily: 'Exo', fontWeight: '500', wordWrap: 'break-word'}}>Select</div>
+  </div>
+  <div style={{width: 303, height: 585, left: 1004, top: 1265, position: 'absolute'}}>
+    <div style={{width: 303, left: 0, top: 0, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 36, fontFamily: 'Exo', fontWeight: '600', wordWrap: 'break-word'}}>ROG Flow Z13 (2023)</div>
+    <div style={{width: 269, left: 17, top: 135, position: 'absolute', textAlign: 'center', color: '#F5F5F5', fontSize: 20, fontFamily: 'Exo', fontWeight: '600', wordWrap: 'break-word'}}>NVIDIA® GeForce RTX™ 4050 Laptop GPU<br/>Windows 11 Home<br/>13th Gen Intel® Core™ i9-13900H Processor<br/>13.4" QHD+ (2560 x 1600, WQXGA) 16:10 165Hz ROG Nebula Display<br/>1TB M.2 NVMe™ PCIe® 4.0 SSD storage (2230)</div>
+    <div style={{left: 65, top: 452, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 40, fontFamily: 'Exo', fontWeight: '700', wordWrap: 'break-word'}}>$1,749.99</div>
+    <div style={{width: 200, height: 60, left: 51, top: 525, position: 'absolute', background: '#007FFF', borderRadius: 16}} />
+    <div style={{left: 106, top: 533, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 32, fontFamily: 'Exo', fontWeight: '500', wordWrap: 'break-word'}}>Select</div>
+  </div>
+  <div style={{width: 1440, height: 400, left: 0, top: 3541, position: 'absolute'}}>
+    <div style={{width: 1440, height: 400, left: 0, top: 0, position: 'absolute', background: 'black'}} />
+    <div style={{width: 321, height: 43, left: 13, top: 37, position: 'absolute'}}>
+      <div style={{left: 141, top: 6, position: 'absolute', textAlign: 'center', color: '#7E7E7E', fontSize: 24, fontFamily: 'Exo', fontWeight: '400', wordWrap: 'break-word'}}>Gaming Laptops</div>
+      <div style={{width: 152, height: 43, left: 0, top: 0, position: 'absolute', textAlign: 'center', color: '#007FFF', fontSize: 40, fontFamily: 'Exo', fontWeight: '600', wordWrap: 'break-word'}}>AZUR</div>
     </div>
+    <div style={{left: 116, top: 123, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Exo', fontWeight: '400', wordWrap: 'break-word'}}>Products</div>
+    <div style={{left: 267, top: 123, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Exo', fontWeight: '400', wordWrap: 'break-word'}}>About</div>
+    <div style={{left: 386, top: 123, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Exo', fontWeight: '400', wordWrap: 'break-word'}}>Help</div>
+    <div style={{left: 499, top: 341, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Exo', fontWeight: '400', wordWrap: 'break-word'}}>AZUR COMP INC, ALL RIGHTS RESERVED.</div>
+    <div style={{width: 600, height: 5, left: 0, top: 159, position: 'absolute', background: '#007FFF', borderRadius: 20}} />
+    <div style={{width: 600, height: 5, left: 840, top: 231, position: 'absolute', background: '#007FFF', borderRadius: 20}} />
+    <img style={{width: 60, height: 60, left: 1067, top: 242, position: 'absolute'}} src="https://via.placeholder.com/60x60" />
+    <img style={{width: 60, height: 60, left: 1174, top: 242, position: 'absolute'}} src="https://via.placeholder.com/60x60" />
+  </div>
+  <div style={{width: 1440, height: 504, left: 0, top: 2046, position: 'absolute'}}>
+    <img style={{width: 1440, height: 504, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/1440x504" />
+    <img style={{width: 1440, height: 504, left: 2946, top: 0, position: 'absolute'}} src="https://via.placeholder.com/1440x504" />
+    <img style={{width: 1440, height: 504, left: 1473, top: 0, position: 'absolute'}} src="https://via.placeholder.com/1440x504" />
+  </div>
+  <div style={{width: 619, left: 410, top: 2602, position: 'absolute', textAlign: 'center'}}><span style={{color: '#007FFF', fontSize: 36, fontFamily: 'Exo', fontWeight: '700', wordWrap: 'break-word'}}>Refine Your Gaming Experience:</span><span style={{color: '#007FFF', fontSize: 32, fontFamily: 'Exo', fontWeight: '700', wordWrap: 'break-word'}}> <br/>Tailor your Search with Precision</span></div>
+  <div style={{width: 200, height: 60, left: 620, top: 2716, position: 'absolute', background: 'black', borderRadius: 16}}></div>
+  <div style={{left: 681, top: 2730, position: 'absolute', textAlign: 'center', color: '#007FFF', fontSize: 24, fontFamily: 'Exo', fontWeight: '700', wordWrap: 'break-word'}}>FILTER</div>
+  <div style={{width: 400, height: 500, left: 973, top: 2971, position: 'absolute', background: '#333333', borderRadius: 20}} />
+  <div style={{width: 400, height: 500, left: 951, top: 2857, position: 'absolute', background: '#333333', borderRadius: 20}} />
+  <div style={{width: 400, height: 500, left: 89, top: 2857, position: 'absolute', background: 'rgba(51, 51, 51, 0.20)', borderRadius: 20}} />
+  <img style={{width: 300, height: 239.94, left: 570, top: 2857, position: 'absolute'}} src="https://via.placeholder.com/300x240" />
+  <img style={{width: 300, height: 239.94, left: 1001, top: 2857, position: 'absolute'}} src="https://via.placeholder.com/300x240" />
+  <img style={{width: 300, height: 239.94, left: 139, top: 2857, position: 'absolute'}} src="https://via.placeholder.com/300x240" />
+  <div style={{width: 344, left: 548, top: 3119, position: 'absolute', textAlign: 'center'}}><span style={{color: '#007FFF', fontSize: 32, fontFamily: 'Exo', fontWeight: '700', wordWrap: 'break-word'}}>Raider 18 HX A14V<br/><br/></span><span style={{color: 'white', fontSize: 20, fontFamily: 'Exo', fontWeight: '500', wordWrap: 'break-word'}}>Intel® Core™ processor (14th gen) / GeForce RTX™ 40 Series</span></div>
+  <div style={{width: 344, left: 117, top: 3119, position: 'absolute', textAlign: 'center'}}><span style={{color: '#007FFF', fontSize: 32, fontFamily: 'Exo', fontWeight: '700', wordWrap: 'break-word'}}>Titan 18 HX A14V<br/><br/></span><span style={{color: 'white', fontSize: 20, fontFamily: 'Exo', fontWeight: '500', wordWrap: 'break-word'}}>Intel® Core™ processor (14th gen) / GeForce RTX™ 40 Series</span></div>
+  <div style={{width: 344, left: 979, top: 3119, position: 'absolute', textAlign: 'center'}}><span style={{color: '#007FFF', fontSize: 32, fontFamily: 'Exo', fontWeight: '700', wordWrap: 'break-word'}}>Vector 17 HX A14V<br/><br/></span><span style={{color: 'white', fontSize: 20, fontFamily: 'Exo', fontWeight: '500', wordWrap: 'break-word'}}>Intel® Core™ processor (14th gen) / GeForce RTX™ 40 Series</span></div>
+  <img style={{width: 60, height: 60, left: 1282, top: 3783, position: 'absolute'}} src="https://via.placeholder.com/60x60" />
+  <div style={{width: 1440, height: 153, left: 0, top: 0, position: 'absolute'}}>
+    <div style={{width: 1440, height: 80, left: 0, top: 0, position: 'absolute', background: '#333333'}} />
+    <div style={{width: 152, height: 43, left: 37, top: 19, position: 'absolute', textAlign: 'center', color: '#007FFF', fontSize: 40, fontFamily: 'Exo', fontWeight: '700', wordWrap: 'break-word'}}>AZUR</div>
+    <img style={{width: 30, height: 30, left: 1331, top: 25, position: 'absolute'}} src="https://via.placeholder.com/30x30" />
+    <div style={{width: 120, height: 39, left: 887, top: 21, position: 'absolute'}}>
+      <div style={{width: 120, height: 39, left: 0, top: 0, position: 'absolute', background: '#333333', borderRadius: 10}} />
+      <div style={{width: 73, height: 25, left: 23, top: 7, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Exo', fontWeight: '500', wordWrap: 'break-word'}}>Home</div>
+    </div>
+    <div style={{width: 165, height: 39, left: 1008, top: 21, position: 'absolute'}}>
+      <div style={{width: 165, height: 39, left: 0, top: 0, position: 'absolute', background: '#333333', borderRadius: 10}} />
+      <div style={{width: 100.38, height: 25, left: 31.62, top: 7, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Exo', fontWeight: '500', wordWrap: 'break-word'}}>Products</div>
+    </div>
+    <div style={{width: 132, height: 129, left: 1179, top: 24, position: 'absolute'}}>
+      <div style={{width: 132, height: 30, left: 0, top: 0, position: 'absolute', background: '#333333'}} />
+      <div style={{width: 126, height: 30, left: 2, top: 0, position: 'absolute'}}>
+        <div style={{width: 83, height: 25, left: 27, top: 3, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Exo', fontWeight: '500', wordWrap: 'break-word'}}>Profile</div>
+        <img style={{width: 30, height: 30, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/30x30" />
+        <img style={{width: 20, height: 20, left: 106, top: 5, position: 'absolute'}} src="https://via.placeholder.com/20x20" />
+      </div>
+    </div>
+  </div>
+</div>
   );
 }
 
